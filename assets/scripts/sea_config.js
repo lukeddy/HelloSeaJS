8ac992ad2663cfe7735641cb9e6cdea06e05d917
tzq668766 <tzq618766@gmail.com>
2014-07-28 17:57:00 +0800

    添加seajs配置文件 添加seajs-text插件 添加jquery库

---
+++ assets/scripts/sea_config.js
@@ -0,0 +1,43 @@
+seajs.config({
+	// 别名配置
+	alias: {
+		'seajs-text' : 'assets/scripts/seajs/seajs-text/1.1.1/seajs-text',
+		'jquery'     : 'assets/scripts/jquery/1.9.1/jquery',
+		
+		'util'     : 'assets/scripts/application/util',
+		'application' :'assets/scripts/application/application'
+	},
+	map: [
+
+		[ /^(.*\.(?:css|js|tpl))(.*)$/i, '$1?'+seajsTimestamp ]
+	],
+
+	// 路径配置
+	//paths: {
+	//	'gallery': 'https://a.alipayobjects.com/gallery'
+	//},
+
+	// 变量配置
+	//vars: {
+	//	'locale': 'zh-cn'
+	//},
+
+	// 映射配置
+	//map: [
+	//	['http://example.com/js/app/', 'http://localhost/js/app/']
+	//],
+
+	// 预加载项
+
+	preload: ['jquery', 'seajs-text'],
+	//preload: ['jquery', 'seajs-text', 'seajs-combo'],
+
+	// 调试模式
+	debug: true,
+
+	// Sea.js 的基础路径
+	base: 'http://localhost/HelloSeaJS/',
+
+	// 文件编码
+	charset: 'utf-8'
+});
